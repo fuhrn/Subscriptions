2a43a7cf1b536447ad37bd48441447d6482b4d76
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-08-13 13:14:02 -0300

    Creating and listing plans

---
+++ app/Http/Controllers/Subscriptions/PlanController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Subscriptions;
+
+use App\Http\Controllers\Controller;
+use App\Plan;
+use Illuminate\Http\Request;
+
+class PlanController extends Controller
+{
+    public function index()
+    {
+        $plans = Plan::get();
+
+        return view('subscriptions.plans', compact('plans'));
+    }
+}
