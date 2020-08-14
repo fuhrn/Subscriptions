79471f48826a420fc7cc5a81dac8ccb2a922c4f5
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-08-13 22:00:54 -0300

    Subscription status middleware

--- app/Http/Controllers/Subscriptions/SubscriptionController.php
+++ app/Http/Controllers/Subscriptions/SubscriptionController.php
@@ -10,7 +10,7 @@ class SubscriptionController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth']);
+        $this->middleware(['auth', 'not.subscribed']);
     }
 
     public function index(Request $request)
