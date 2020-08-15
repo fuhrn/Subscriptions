59b8454ec849b668fe2f364c9cf54d12805f4aac
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-08-15 20:20:04 -0300

    Handling SCA when swapping

--- app/Http/Controllers/Account/Subscriptions/SubscriptionSwapController.php
+++ app/Http/Controllers/Account/Subscriptions/SubscriptionSwapController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Account\Subscriptions;
 use App\Http\Controllers\Controller;
 use App\Plan;
 use Illuminate\Http\Request;
+use Laravel\Cashier\Exceptions\PaymentActionRequired;
 
 class SubscriptionSwapController extends Controller
 {
@@ -21,8 +22,19 @@ class SubscriptionSwapController extends Controller
             'plan' => 'required|exists:plans,slug'
         ]);
 
-        $request->user()->subscription('default')
-            ->swap(Plan::where('slug', $request->plan)->first()->stripe_id);
+
+
+        try {
+            $request->user()->subscription('default')
+                ->swap(Plan::where('slug', $request->plan)->first()->stripe_id);
+        } catch (PaymentActionRequired $e) {
+            return redirect()->route(
+                'cashier.payment', [
+                    $e->payment->id,
+                    'redirect' =>route('account.subscriptions')
+                ]
+            );
+        }
 
         return redirect()->route('account.subscriptions');
     }
